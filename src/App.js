81752eae2fbd314582666ae642590904395e423d
hossam <hosamabdulmaged@gmail.com>
2021-04-28 15:26:10 +0200

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [anArray, setanArray] = useState([1, 2, 3, 4, 5, 6, 7, 9, 9]);
+
+  const findMissingNumber = anArray => {
+    for (let i = 0; i < anArray.length - 1; i++) {
+      if (anArray[i] + 1 != anArray[i + 1]) {
+        return anArray[i] + 1;
+      }
+    }
+    return false;
+  }
+
+  console.log(findMissingNumber(anArray), `findMissingNumber(anArray)`)
+
+
+  const [dupes, setDupes] = useState([1, 2, 3, 4, 5, 5, 6, 7, 8]);
+
+  const findDupes = dupes => {
+    let anObject = {};
+    for (let i = 0; i < dupes.length; i++) {
+      if (anObject[dupes[i]]) {
+        return dupes[i]
+      } else {
+        anObject[dupes[i]] = dupes[i]
+      }
+    }
+    return false;
+  }
+
+
+  console.log(findDupes(dupes), `fundDupes(dupes)`)
+
+
+
+  const [minMax, setMinMax] = useState([1, 2, 3, 4, 5, 600]);
+
+  const findMaxMin = (minMax) => {
+    let min = minMax[0];
+    let max = minMax[0]
+
+
+    for (let i = 0; i < minMax.length; i++) {
+      if (anArray[i] > max) {
+        max = minMax[i]
+      } else if (anArray[i] < min) {
+        min = minMax[0]
+      }
+    }
+    return {
+      'min': min,
+      'max': max,
+    }
+  }
+  console.log(findMaxMin(minMax), `findMaxMin(minMax)`)
+
+
+
+  const [cumulative, setCumulative] = useState([1, 3, 5, 7]);
+
+  const cumulativeSum = list => {
+    let result = [list[0]];
+    for (let i = 1; i < list.length; i++) {
+      result.push(list[i] + result[i - 1]);
+    }
+    return result
+  }
+
+  console.log(cumulativeSum(cumulative), `cumulativeSum(cumulative)`)
+
+
+  const [multiDupes, setMultiDupes] = useState([1, 1, 2, 3, 4, 5, 6, 7, 8, 6, 6, 7, 7, 7, 10, 10]);
+
+  const returnMultipleDupes = multiDupes => {
+    let anObject = {};
+    let anArray = [];
+
+    for (let i = 0; i < multiDupes.length; i++) {
+
+      if (anObject[multiDupes[i]]) {
+
+        if (anObject[multiDupes[i]] === 1) {
+
+          anArray.push(multiDupes[i]);
+        }
+        anObject[multiDupes[i]] = anObject[multiDupes[i]] + 1;
+      } else {
+        anObject[multiDupes[i]] = 1;
+      }
+    }
+    return anArray;
+  }
+
+  console.log(returnMultipleDupes(multiDupes), `returnMultipleDupesanArrayay(multiDupes)`)
+
+  // Questing 6. Remove all duplicates from an array of integers
+
+
+  const [removeDupesIndex, setRemoveDupesIndex] = useState([1, 1, 1, 1, 1, 1, 1]);
+
+  const removeDupes = removeDupesIndex => {
+    let result = [];
+    let previous = removeDupesIndex[0];
+    result[0] = previous;
+    for (let i = 0; i < removeDupesIndex.length; i++) {
+      if (removeDupesIndex !== previous) {
+        result.push(removeDupesIndex[i])
+      }
+      previous = removeDupesIndex[i];
+    }
+    return result;
+  }
+
+  console.log(removeDupes(removeDupesIndex), `removeDupes(removeDupesIndex)`)
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Solutions
+     {/*   <br />
+        {returnDupInNewanArray(u)}
+        <hr />
+        {anObjectject.values(findMinAndMax(minMax))}
+        {anArrayr.map((x) => x)}
+     */} </header>
     </div>
   );
 }
-
 export default App;
