fdcd2aae6bfba46cdc9a58577d7137e04adc8271
hossam <hosamabdulmaged@gmail.com>
2021-04-29 15:51:43 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -135,10 +135,131 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h3>
-          Solutions
+          Algorithm Interview Questions and Answers (JS)
         </h3>
       </header>
     </div>
-  );
+  )
 }
+
 export default App;
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 4, 8, 9, 1000];
+
+const findMissingNumberس = arr => {
+  if (arr[0] === 1 & arr[arr.length - 1] === 100) {
+    console.log(`do not need itrate for Loop and Return the missing number`)
+    return arr;
+  } else {
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] + 1 !== arr[i + 1]) {
+        return arr[i] + 1;
+      }
+    }
+  }
+  return false;
+}
+
+
+
+const findDupesz = arr => {
+  let observer = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (observer[arr[i]]) {
+      //  console.log(observer[arr[i]], `observer[arr[i]]`)
+      return arr[i];
+    } else {
+      //   console.log(arr[i], `arr[i]`)
+      observer[arr[i]] = arr[i];
+    }
+  }
+  console.log(`no Dupes Number Found`);
+  return false;
+
+}
+
+console.log(findDupesz(arr), `findDupesz(arr)`)
+
+console.log(findMissingNumberس(arr), `findMissingNumber(arr)`)
+
+
+const FindMinandMax = arr => {
+  let min = arr[0];
+  let max = arr[0];
+  // using itrate loop to get [min && max]
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i]
+    }
+  }
+  return {
+    'min': min,
+    'max': max,
+  }
+}
+
+console.log(FindMinandMax(arr), `FindMinandMax`);
+
+let Ax = [1, 2, 3, 4];
+
+
+const CumlativeSum = list => {
+  let result = [list[0]]
+  for (let i = 1; i < list.length; i++) {
+    result.push(list[i] + result[i - 1])
+  }
+  return result;
+}
+
+console.log(CumlativeSum(Ax), `CumlativeSum(Ax)`);
+
+
+const Axx = [1, 1, 2, 3, 4, 5, 6, 7, 8, 6, 6, 7, 7, 7, 10, 10];
+
+
+
+const returnMultipleDupesArrayz = Axx => {
+  let anObject = {};
+  let anArray = [];
+  for (let i = 0; i < Axx.length; i++) {
+    if (anObject[Axx[i]]) {
+      if (anObject[Axx[i]] === 1) {
+        anArray.push(Axx[i])
+      }
+      anObject[Axx[i]] = anObject[Axx[i]] + 1;
+    } else {
+      anObject[Axx[i]] = 1;
+    }
+  }
+  return anArray;
+}
+
+console.log(returnMultipleDupesArrayz(Axx), `returnMultipleDupesArrayz(Axx)`)
+
+
+
+let remove = [1, 1, 1, 1, 1, 1];
+
+
+const removeDupes = remove => {
+  let result = [];
+
+  let prev = remove[0];
+
+  result[0] = prev;
+
+  for (let i = 0; i < remove.length; i++) {
+    if (remove[i] !== prev) {
+      result.push(remove[i]);
+    } 
+    else {
+      prev = remove[i]
+    }
+  }
+  return result;
+}
+
+console.log(removeDupes(remove), `removeDupes(remove)`)
